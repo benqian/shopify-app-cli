fef079aad2d7580206c7a3297c4af983de8f5cc1
Konstantin Tennhard <konstantin.tennhard@shopify.com>
2021-02-03 19:28:31 -0500

    Specifications -> Models::Specifications

--- lib/project_types/extension/cli.rb
+++ lib/project_types/extension/cli.rb
@@ -67,64 +67,13 @@ module Extension
     autoload :Version, Project.project_filepath('models/version')
     autoload :Type, Project.project_filepath('models/type')
     autoload :ValidationError, Project.project_filepath('models/validation_error')
+    autoload :Specifications, Project.project_filepath('models/specifications')
   end
 
   autoload :ExtensionProjectKeys, Project.project_filepath('extension_project_keys')
   autoload :ExtensionProject, Project.project_filepath('extension_project')
 
-  class Specifications
-    def initialize
-      @repository = load_all_types
-    end
-
-    def [](identifier)
-      repository[identifier]
-    end
-
-    def valid?(identifier)
-      repository.key?(identifier)
-    end
-
-    def each(&block)
-      enum_for(:each) if block.nil?
-      repository.values.each(&block)
-    end
-
-    def register(type)
-      repository[type.identifier] = type
-    end
-
-    def unregister(type)
-      repository.delete(type.identifier)
-    end
-
-    protected
-
-    attr_reader :repository
-
-    private
-
-    def load_all_types
-      search_pattern = %w[lib project_types extension models types *.rb]
-      Dir.glob(File.join(ShopifyCli::ROOT, search_pattern)).each_with_object({}) do |path, repository|
-        require(path)
-
-        build_type(infer_type_name_from_path(path)).tap do |type|
-          repository[type.identifier] = type
-        end
-      end
-    end
-
-    def infer_type_name_from_path(path)
-      File.basename(path, '.rb').split('_').map(&:capitalize).join
-    end
-
-    def build_type(class_name)
-      Extension::Models::Types.const_get(class_name).new
-    end
-  end
-
   def self.specifications
-    @specifications ||= Specifications.new
+    @specifications ||= Models::Specifications.new
   end
 end
